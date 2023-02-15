32b3226f096632a2cf2d1a60ff27ea54ea341aa5
magoga <patt8i@hotmail.com>
2023-02-15 09:49:03 -0300

    Subida de carpetas de frontend

---
+++ backend/app-libreria/src/main/java/com/home/applibreria/AppLibreriaApplication.java
@@ -0,0 +1,13 @@
+package com.home.applibreria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppLibreriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppLibreriaApplication.class, args);
+	}
+
+}
---
+++ backend/app-libreria/src/test/java/com/home/applibreria/AppLibreriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.home.applibreria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppLibreriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
